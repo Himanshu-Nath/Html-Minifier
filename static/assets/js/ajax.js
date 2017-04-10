cb233d3c65c1458b053b0af33f1091d40216b960
Himanshu Nath <hnath723@gmail.com>
2017-04-10 20:23:22 +0530

    Compress done

--- static/assets/js/ajax.js
+++ static/assets/js/ajax.js
@@ -4,5 +4,25 @@ function resetAllField() {
 }
 
 function compress() {
-	
+	var inputText = document.getElementById('inputBox').value;
+	var reqSend = {};
+	reqSend.value = 001;
+	reqSend.data = inputText;
+	console.log(inputText);
+	$(document).ready(function() {
+    $.ajax({
+        url: 'http://localhost:3000/compress',
+		data: JSON.stringify(reqSend),
+		contentType: 'application/json',
+		type: "POST",
+        success: function(respGet) {
+            console.log("Success");
+			console.log(JSON.stringify(respGet));
+			document.getElementById("outputBox").value = respGet.data;
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('Error: ' + errorThrown);
+        }
+    });
+});
 }
