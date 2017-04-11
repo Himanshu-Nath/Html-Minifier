f8e4ce9ee4d0c1ad8a6f91aad50c3e90bbbbb7ee
Himanshu-Nath <hnath723@gmail.com>
2017-04-11 16:32:01 +0530

    Uncompression done

--- static/assets/js/ajax.js
+++ static/assets/js/ajax.js
@@ -1,28 +1,65 @@
+$(window).ready( function() {
+  $(".alertBox").hide();
+  $('.alertBoxSuccess').hide();
+});
+
 function resetAllField() {
 	form = document.getElementById("inputBox").value = "";		//One Method
 	$("#outputBox").val('');									//Second Method
+	$(".alertBox").hide();
+	$('.alertBoxSuccess').hide();
 }
 
 function compress() {
 	var inputText = document.getElementById('inputBox').value;
-	var reqSend = {};
-	reqSend.value = 001;
-	reqSend.data = inputText;
-	console.log(inputText);
-	$(document).ready(function() {
-    $.ajax({
-        url: 'http://localhost:3000/compress',
-		data: JSON.stringify(reqSend),
-		contentType: 'application/json',
-		type: "POST",
-        success: function(respGet) {
-            console.log("Success");
-			console.log(JSON.stringify(respGet));
-			document.getElementById("outputBox").value = respGet.data;
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log('Error: ' + errorThrown);
-        }
-    });
-});
+	if(inputText.length != 0){
+		var reqSend = {};
+		reqSend.value = 001;
+		reqSend.data = inputText;
+		$(document).ready(function() {
+			$.ajax({
+				url: 'http://localhost:3000/compress',
+				data: JSON.stringify(reqSend),
+				contentType: 'application/json',
+				type: "POST",
+				success: function(respGet) {
+					$('.alertBoxSuccess').show();
+					document.getElementById("outputBox").value = respGet.data;
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.log('Error: ' + errorThrown);
+				}
+			});
+		});
+	} else {
+		$('.alertBox').show();
+	}
+}
+
+function uncompress() {
+	var type = document.getElementById('uncompressionType').value;
+	var inputText = document.getElementById('inputBox').value;
+	if(inputText.length != 0){
+		var reqSend = {};
+		reqSend.value = 001;
+		reqSend.data = inputText;
+		reqSend.type = type;
+		$(document).ready(function() {
+			$.ajax({
+				url: 'http://localhost:3000/uncompress',
+				data: JSON.stringify(reqSend),
+				contentType: 'application/json',
+				type: "POST",
+				success: function(respGet) {
+					$('.alertBoxSuccess').show();
+					document.getElementById("outputBox").value = respGet.data;
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.log('Error: ' + errorThrown);
+				}
+			});
+		});
+	} else {
+		$('.alertBox').show();
+	}
 }
